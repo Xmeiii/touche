5d98a27da82b9036300583c90fea7f2cb9231473
Tran Ngo Sao Mai <120165712+Xmeiii@users.noreply.github.com>
2023-12-16 22:35:04 +0700

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@ $message = $_POST['message'];
 $to = 'tranngosaomai@gmail.com'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@touche.com";
+$headers = "From: noreply@xmeiii.github.io.com";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
